962822f93eed8c1e76d73104a766ed862b957c73
ivanda12 <85473514+ivanda12@users.noreply.github.com>
2023-11-15 21:32:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Menjalankan perintah curl dan menyimpan outputnya ke dalam file Update.tsx
-curl_output=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=ADDRESSMU 2>&1)
+curl_output=$(curl -k -f https://pilotdog.tech/api/get_eligible_proof?address=0x6971E87D8D4FF48b8C292380aBF57F54F83Ef764 2>&1)
 
 # Menyimpan hasil output curl ke dalam file Update.tsx
 echo "$curl_output" >> Update.tsx
