548b62ac42ac6db43b6f295d30b2eefd25c9d2db
ivanda12 <ieiem.community@gmail.com>
2023-11-15 15:29:16 +0000

    🟤 auto commit

--- Update.tsx
+++ Update.tsx
@@ -363,3 +363,7 @@ curl: (22) The requested URL returned error: 408
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    423      0 --:--:-- --:--:-- --:--:--   425
 {"code":"InvalidParameters","error":"not found","data":null}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    316      0 --:--:-- --:--:-- --:--:--   317
+{"code":"InvalidParameters","error":"not found","data":null}
